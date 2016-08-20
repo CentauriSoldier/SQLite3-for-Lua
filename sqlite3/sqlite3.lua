5d561e7a6a581badbd1f3d03d414c80a1da7a939
Centauri Soldier <centaurisoldier@gmail.com>
2016-08-19 17:04:39 -0700

    Compatibility Patch
    Added compatibility for linux, windows and mac as well as automated most
    of the script.

--- sqlite3/sqlite3.lua
+++ sqlite3/sqlite3.lua
@@ -1,8 +1,23 @@
+--[[
+The MIT License (MIT)
+
+Copyright (c) 2016 Centauri Soldier https://github.com/CentauriSoldier/SQLite3-LOVE
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+DLLs provided by josefnpat
+https://love2d.org/forums/viewtopic.php?f=5&t=38486&hilit=sqlite3
+]]
+
 local tSettings = {
 	SQLite3Path = "plugins/sqlite3", --Edit	to suit your project: this is case-sensitive on linux!
 	SystemType = "Windows", --Edit to suit your project
 	SystemBits = "64", --Edit to suit your project
-	--<<<< DO NOT EDIT BELOW THIS LINE >>>>
+	--<<<< DO NOT EDIT BELOW THIS LINE >>>> <<<< DO NOT EDIT BELOW THIS LINE >>>> <<<< DO NOT EDIT BELOW THIS LINE >>>> 
 	SystemPaths = { 
 		linux = {
 			["32"] = "dll/linux_x86/lsqlite3",
@@ -18,13 +33,6 @@ local tSettings = {
 		},
 	},
 };
-
-
---[[*
-@module sqlite3
-DLLs provided by josefnpat
-https://love2d.org/forums/viewtopic.php?f=5&t=38486&hilit=sqlite3
-*]]
 
 local function SQLite3Error(sError)
 error("SQLite3 init error: "..sError);
